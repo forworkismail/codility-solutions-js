303474c2adb304285119342948e4a5caa3d471fb
Ismail <forwork.ismail@gmail.com>
2022-02-08 16:38:05 +0430

    Lesson4-CountingElements/10.Missing-Integer.js

---
+++ Lesson4-CountingElements/10.Missing-Integer.js
@@ -0,0 +1,15 @@
+const findSmallestPositiveInteger = (A) => {
+  if (A.length >= 1 && A.length <= 100000) {
+    const sortedArray = A.sort((a, b) => a - b);
+    let output = 1;
+
+    for (let i = 0; i < sortedArray.length; i++) {
+      if (A[i] === output) {
+        output++;
+      }
+    }
+    console.log(output)
+    return output;
+  }
+  return 0;
+}
